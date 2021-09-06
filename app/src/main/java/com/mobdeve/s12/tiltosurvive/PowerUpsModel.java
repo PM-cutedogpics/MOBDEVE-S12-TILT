b157a2c7b844f8de2267b915d38d94916040690e
PM-cutedogpics <pete_choi@dlsu.edu.ph>
2021-09-07 01:55:47 +0800

    Revert "Revert "changed store fixed values to recycler view.""
    This reverts commit 9c7f569b73a619a1a796e1840c4b8a4ef6b8fb13.

--- app/src/main/java/com/mobdeve/s12/tiltosurvive/PowerUpsModel.java
+++ app/src/main/java/com/mobdeve/s12/tiltosurvive/PowerUpsModel.java
@@ -8,16 +8,17 @@ public class PowerUpsModel {
     private int imageId;
     private int activatedImageId;
     private int isSelected;
-    private int owned;
+    private int isOwned;
+    private int price;
 
-    public PowerUpsModel(String title, String description, int imageId, int activatedImageId, int isSelected, int owned) {
+    public PowerUpsModel(String title, String description, int imageId, int activatedImageId, int isSelected, int isOwned, int price) {
         this.title = title;
         this.description = description;
         this.imageId = imageId;
         this.activatedImageId = activatedImageId;
         this.isSelected = isSelected;
-        this.owned = owned;
-
+        this.isOwned = isOwned;
+        this.price = price;
     }
 
     public String getTitle() {
@@ -45,13 +46,21 @@ public class PowerUpsModel {
     }
 
     public Integer getOwned() {
-        return owned;
+        return isOwned;
     }
 
-    public void setOwned(Integer owned) {
-        this.owned = owned;
+    public void setOwned(Integer isOwned) {
+        this.isOwned = isOwned;
     }
 
     public int getSelected() { return this.isSelected;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
